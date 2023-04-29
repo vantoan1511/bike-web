1423ce3ba2e38cc355db91b9f2c18ec336a52f4a
NGUYEN VAN TOAN <vantoan1517@gmail.com>
2023-04-29 09:23:34 +0700

    them file sp

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // show & hidden password
-var showHiddenPW = function (loginPass, loginEye) {
+function showHiddenPW(loginPass, loginEye) {
     var input = document.getElementById(loginPass),
         iconEye = document.getElementById(loginEye);
     iconEye.addEventListener('click', function () {
@@ -16,8 +16,6 @@ var showHiddenPW = function (loginPass, loginEye) {
     })
 }
 
-showHiddenPW('input-password', 'password-eye');
-showHiddenPW('re-input-password', 're-password-eye');
 
 // shopping cart
 
@@ -35,12 +33,12 @@ var itemList = {
         'name': 'Xe B',
         'price': '1500',
         'photo': 'img/xe-a.jpg'
-    }, 
+    },
     'sp003': {
         'name': 'Xe C',
         'price': '2000',
         'photo': 'img/xe-a.jpg'
-    }, 
+    },
     'sp004': {
         'name': 'Xe D',
         'price': '2500',
@@ -51,19 +49,25 @@ var itemList = {
 function addCart(code) {
     var number = parseInt(document.getElementById(code).value);
     var current = parseInt(window.localStorage.getItem(code));
-    if(typeof localStorage[code] === 'undefined') {
+    if (typeof localStorage[code] === 'undefined') {
         window.localStorage.setItem(code, number);
     }
     else {
-        if(number+current > 100) {
+        if (number + current > 100) {
             window.localStorage.setItem(code, 100);
         }
-        else{
-            window.localStorage.setItem(code,number+current);
+        else {
+            window.localStorage.setItem(code, number + current);
         }
     }
 }
 
 function showCart() {
-        
+
 }
+
+$(document).ready(function () {
+    showHiddenPW('input-password', 'password-eye');
+    showHiddenPW('re-input-password', 're-password-eye');
+    console.log('tét');
+});
